51f8ef749ad28d7c3a052bae7d4d200dad2d968d
Brigadira1 <iavor.stoimenov@yahoo.com>
2024-07-19 14:15:09 +0300

    Scripts and NvChad folders were added

---
+++ scripts/add_arch_packages.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+PACKAGES="" 
+AUR_PACKAGES=""
+INSTALLER_OPTIONS=" --needed --noconfirm" 
+
+install_pacman_packages() {
+
+  PACKAGES+="qemu-guest-agent git lshw openssh xrdp lxappearance arc-gtk-theme wget curl rsync neovim xclip gzip zip unzip libx11 libxft harfbuzz libxinerama cups hplip ntp nfs-utils cifs-utils htop net-tools vifm plocate bash-completion lsd alacritty starship xorg-xrandr python python-pip python-psutil network-manager-applet reflector linux  -headers tree less rofi picom nitrogen hwinfo glibc linux-headers"
+
+  echo "Executing pacman with a preconfigured list of packages...."
+  pacman -S $PACKAGES $INSTALLER_OPTIONS
+}
+
+install_yay_packages() {
+  AUR_PACKAGES+="brave-bin xorg-xdpyinfo xorgxrdp-nvidia papirus-icon-theme-gi qtile-extras nomachine pavucontrol qt5ctarchlinux-tweak-tool-git xorgxrdp-nvidia"
+
+  echo "Executing yay with a preconfigured list of packages...."
+  yay -S $AUR_PACKAGES $INSTALLER_OPTIONS
+}
+
+configure_services() {
+
+  echo "Enabling sshd service..."
+  systemctl enable --now sshd
+
+  echo "Enabling xrdp service..."
+  systemctl enable --now xrdp
+
+  echo "Enabling printer service..."
+  systemctl enable --now cups.service
+
+  echo "Enabling time sync service..."
+  systemctl enable --now ntpd
+
+  echo "Enabling NoMachine service..."
+  systemctl enable --now nxserver.service
+
+  echo "Adding Bulgaria as the location for the mirror list of repositories..."
+  reflector --country Bulgaria --age 6 --sort rate --save /etc/pacman.d/mirrorlist
+  echo "Enabling the reflector service..."
+  systemctl enable --now reflector.timer
+}
+
+install_pacman_packages
+install_yay_packages
+configure_services
+
+
+
---
+++ scripts/main.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+ARCH_PACKAGES_SCRIPT=add_arch_packages.sh
+CUSTOM_CONF_SCRIPT=custom_conf_and_dot_files.sh
+
+if [[ -f "./$ARCH_PACKAGES_SCRIPT"  &&  -x "./$ARCH_PACKAGES_SCRIPT"  && -f "./$CUSTOM_CONF_SCRIPT" && -x "./$CUSTOM_CONF_SCRIPT" ]]; then
+  . ./$ARCH_PACKAGES_SCRIPT && . ./$CUSTOM_CONF_SCRIPT
+fi
