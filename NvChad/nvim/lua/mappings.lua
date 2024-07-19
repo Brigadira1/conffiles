191f33721472e2d6240d518d6e21ec3d22e1528f
Brigadira1 <iavor.stoimenov@yahoo.com>
2024-07-19 20:56:56 +0300

    Changes to the config of NvChad

--- NvChad/nvim/lua/mappings.lua
+++ NvChad/nvim/lua/mappings.lua
@@ -2,9 +2,73 @@ require "nvchad.mappings"
 
 -- add yours here
 
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
 local map = vim.keymap.set
+
+--Remap space as leader key
+map("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+
+-- <leader>p|P paste from yank register (0)
+map({ "n", "v" }, "<leader>v", [["+p]], { desc = "paste AFTER from clipboard" })
+map({ "n", "v" }, "<leader>V", [["+P]], { desc = "paste BEFORE from clipboard" })
+map({ "n", "v" }, "<leader>s", [["*p]], { desc = "paste AFTER from primary" })
+map({ "n", "v" }, "<leader>S", [["*P]], { desc = "paste BEFORE from primary" })
+
+-- Turn off search matches with double-<Esc>
+map("n", "<Esc><Esc>", "<Esc>:nohlsearch<CR>", { silent = true })
+
+-- Keep matches center screen when cycling with n|N
+map("n", "n", "nzzzv", { desc = "Fwd  search '/' or '?'" })
+map("n", "N", "Nzzzv", { desc = "Back search '/' or '?'" })
+
+-- Move text up and down
+map("n", "<A-j>", ":m .+1<CR>==", opts)
+map("n", "<A-k>", ":m .-2<CR>==", opts)
+
+-- Keeping the cursor always in the center of the page when C-d and C-u
+map('n', '<C-d>', '<C-d>zz', opts)
+map('n', '<C-u>', '<C-u>zz', opts)
+
+-- Mimic shell movements
+map('i', '<C-E>', '<C-o>$', opts)
+map('i', '<C-A>', '<C-o>^', opts)
+
+-- Move text up and down
+map("n", "<A-k>", ":m .-2<CR>==", opts)
+map("n", "<A-j>", ":m .+1<CR>==", opts)
+
+-- Visual --
+-- Stay in indent mode
+map("v", "<", "<gv^", opts)
+map("v", ">", ">gv^", opts)
+
+-- Move text up and down
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+
+-- Visual Block --
+-- Move text up and down
+map("x", "J", ":m '>+1<CR>gv=gv", opts)
+map("x", "K", ":m '<-2<CR>gv=gv", opts)
+map("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+map("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
