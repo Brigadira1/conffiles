068a7ac125e2ef80613e8ef4c49a310153138641
Brigadira1 <iavor.stoimenov@yahoo.com>
2024-07-22 22:59:06 +0300

    Newest changes in my script files

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,6 +3,8 @@
 ARCH_PACKAGES_SCRIPT=add_arch_packages.sh
 CUSTOM_CONF_SCRIPT=custom_conf_and_dot_files.sh
 
-if [[ -f "./$ARCH_PACKAGES_SCRIPT"  &&  -x "./$ARCH_PACKAGES_SCRIPT"  && -f "./$CUSTOM_CONF_SCRIPT" && -x "./$CUSTOM_CONF_SCRIPT" ]]; then
-  . ./$ARCH_PACKAGES_SCRIPT && . ./$CUSTOM_CONF_SCRIPT
+echo "Trying to source $ARCH_PACKAGES_SCRIPT and $CUSTOM_CONF_SCRIPT"
+
+if [[ -f "./$ARCH_PACKAGES_SCRIPT"  &&  -x "./$ARCH_PACKAGES_SCRIPT"  && -f "./$CUSTOM_CONF_SCRIPT" && -x "./$CUSTOM_CONF_SCRIPT" ]]; then 
+	. ./$ARCH_PACKAGES_SCRIPT && . ./$CUSTOM_CONF_SCRIPT 
 fi
