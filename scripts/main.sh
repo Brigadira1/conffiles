25314b56b289fdaf0c63c61a88ec1634da930754
Brigadira1 <iavor.stoimenov@yahoo.com>
2024-07-23 14:06:25 +0300

    Fixed syntax errors in main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -5,6 +5,6 @@ CUSTOM_CONF_SCRIPT=custom_conf_and_dot_files.sh
 
 echo "Trying to source $ARCH_PACKAGES_SCRIPT and $CUSTOM_CONF_SCRIPT"
 
-if [[ -f "./$ARCH_PACKAGES_SCRIPT"  &&  -x "./$ARCH_PACKAGES_SCRIPT"  && -f "./$CUSTOM_CONF_SCRIPT" && -x "./$CUSTOM_CONF_SCRIPT" ]]; then 
-	. ./$ARCH_PACKAGES_SCRIPT && . ./$CUSTOM_CONF_SCRIPT 
+if [[ -f ./"${ARCH_PACKAGES_SCRIPT}"  &&  -x ./"${ARCH_PACKAGES_SCRIPT}"  && -f ./"${CUSTOM_CONF_SCRIPT}" && -x ./"${CUSTOM_CONF_SCRIPT}" ]]; then 
+	. ./"${ARCH_PACKAGES_SCRIPT}" && . ./"${CUSTOM_CONF_SCRIPT}"
 fi
