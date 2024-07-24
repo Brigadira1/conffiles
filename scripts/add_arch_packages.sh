5f43850520dfd75d16512a9dfe6d4606d614cc3e
Brigadira1 <iavor.stoimenov@yahoo.com>
2024-07-24 13:24:36 +0300

    Installing Hack and Pipewire packages

--- scripts/add_arch_packages.sh
+++ scripts/add_arch_packages.sh
@@ -8,14 +8,14 @@ INSTALLER_OPTIONS=" --needed --noconfirm"
 
 install_pacman_packages() {
 
-  PACKAGES+="base-devel qemu-guest-agent git lshw openssh lxappearance arc-gtk-theme wget curl rsync neovim xclip gzip zip unzip libx11 libxft harfbuzz libxinerama cups hplip ntp nfs-utils cifs-utils htop net-tools vifm plocate bash-completion lsd alacritty starship xorg-xrandr python python-pip python-psutil network-manager-applet reflector tree less rofi picom nitrogen hwinfo glibc linux-headers"
+  PACKAGES+="base-devel qemu-guest-agent git lshw openssh lxappearance arc-gtk-theme wget curl rsync neovim xclip gzip zip unzip libx11 libxft harfbuzz libxinerama cups hplip ntp nfs-utils cifs-utils htop net-tools vifm plocate bash-completion lsd alacritty starship xorg-xrandr python python-pip python-psutil network-manager-applet reflector tree less rofi picom nitrogen hwinfo glibc linux-headers alsa-utils pipewire-alsa pipewire-pulse pipewire-jack"
 
   echo "Executing pacman with a preconfigured list of packages...."
   sudo pacman -S $PACKAGES $INSTALLER_OPTIONS
 }
 
 install_yay_packages() {
-  AUR_PACKAGES+="brave-bin xorg-xdpyinfo xorgxrdp-nvidia papirus-icon-theme-gi qtile-extras nomachine pavucontrol qt5ctarchlinux-tweak-tool-git xorgxrdp-nvidia"
+  AUR_PACKAGES+="brave-bin xorg-xdpyinfo xorgxrdp-nvidia papirus-icon-theme-git qtile-extras nomachine pavucontrol qt5ctarchlinux-tweak-tool-git xorgxrdp-nvidia pipewire-module-xrdp-git"
 
   install_yay
   echo "Executing yay with a preconfigured list of packages...."
@@ -62,13 +62,22 @@ configure_services() {
   echo "Enabling NoMachine service..."
   sudo systemctl enable --now nxserver.service
 
+  echo "Enabling Pipewire services..."
+  sudo systemctl --user --now enable pipewire pipewire-pulse wireplumber
+ 
   echo "Adding Bulgaria as the location for the mirror list of repositories..."
   sudo reflector --country Bulgaria --age 6 --sort rate --save /etc/pacman.d/mirrorlist
   echo "Enabling the reflector service..."
   sudo systemctl enable --now reflector.timer
 }
+install_hack_nerd() {
+    echo "Starting installation of Hack Nerd font family..."
+    sudo pacman -S $INSTALLER_OPTIONS ttf-hack-nerd
+    fc-cache -v 
+}
 
 install_pacman_packages
 install_yay_packages
 configure_services
+install_hack_nerd
 cd "${CURRENT_DIR}"
