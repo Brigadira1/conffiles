a042565a6242d69885cf69752d20baf94d749464
Brigadira1 <iavor.stoimenov@yahoo.com>
2024-07-26 19:26:43 +0300

    dot_os_configs.sh added

--- scripts/dot_os_configs.sh
+++ scripts/dot_os_configs.sh
@@ -8,48 +8,43 @@ XINITRC="$HOME/.xinitrc"
 XPROFILE="$HOME/.xprofile"
 XRESOURCES="$HOME/.Xresources"
 
-HOME_DOT_FILES[$BASHRC]=(
-    "alias ll='lsd -alF'"
-    "alias la='lsd -A'"
-    "alias ls='lsd -al'"
-    "alias vim='nvim'"
-    "export EDITOR=nvim"
-    "export XDG_CONFIG_HOME=/home/brigadira/.config"
-    'export PATH="$XDG_CONFIG_HOME:$PATH"'
-    "colorscript random"
-    'eval "$(starship init bash)"'
+HOME_DOT_FILES[$BASHRC]=$(cat <<'EOF'
+alias ll='lsd -alF'
+alias la='lsd -A'
+alias ls='lsd -al'
+alias vim='nvim'
+export EDITOR=nvim
+export XDG_CONFIG_HOME=/home/brigadira/.config
+export PATH="$XDG_CONFIG_HOME:$PATH"
+colorscript random
+eval "$(starship init bash)"
+EOF
 )
 
-HOME_DOT_FILES[$XPROFILE]=(
-    "nitrogen --restore --set-zoom-fill &"
-    "setxkbmap -model pc104 -layout us,bg -variant ,phonetic -option grp:win_space_toggle &"
-    "picom --config /home/brigadira/.config/picom/picom.conf -f &"
+HOME_DOT_FILES[$XPROFILE]=$(cat <<'EOF'
+nitrogen --restore --set-zoom-fill &
+setxkbmap -model pc104 -layout us,bg -variant ,phonetic -option grp:win_space_toggle &
+picom --config /home/brigadira/.config/picom/picom.conf -f &
+EOF
 )
 
-HOME_DOT_FILES[$XINITRC]=(
-    "nitrogen --restore --set-zoom-fill &"
-    "setxkbmap -model pc104 -layout us,bg -variant ,phonetic -option grp:win_space_toggle &"
-    "xrandr --dpi 109 &"
-    "picom -f &"
-    "exec qtile start"
+HOME_DOT_FILES[$XINITRC]=$(cat <<'EOF'
+nitrogen --restore --set-zoom-fill &
+setxkbmap -model pc104 -layout us,bg -variant ,phonetic -option grp:win_space_toggle &
+xrandr --dpi 109 &
+picom -f &
+exec qtile start
+EOF
 )
 
-HOME_DOT_FILES[$XRESOURCES]=(
-    "Xft.dpi:109"
-)
-
-DOT_FILES=(
-    BASHRC
-    XINITRC
-    XPROFILE
-    )
+HOME_DOT_FILES[$XRESOURCES]="Xft.dpi: 109"
 
 backup_dot_files() {
 
-    for file in "${DOT_FILES[@]}"; do
+    for file in "${!HOME_DOT_FILES[@]}"; do
         if [ -f "$file" ]; then
             echo "$file file exists. Taking a backup..."
-            cp -f "$file" "$file_$(date +%D%M%Y_%H%M%S)"
+            cp -f "$file" "${file}.backup_$(date +%d%m%Y_%H%M%S)"
         else
             echo "$file doesn't exist. No backup was taken."
         fi
@@ -65,11 +60,34 @@ line_exists() {
 
 }
 
-add_line() {
+add_line_to_file() {
 
-    
+    local file=$1
+    local lines="${HOME_DOT_FILES[$file]}"
+
+    while IFS= read -r line; do
+        if [ -n "$line" ]; then
+            if ! line_exists "$file" "$line"; then
+                echo "Adding: $line in $file"
+                echo "$line" >> "$file"
+            else
+                echo "Skipping (already exists): $line in $file"
+            fi
+        fi
+    done <<< "$lines"
 
 }
+add_all_lines_to_all_files() {
+
+    backup_dot_files
+    for file in "${!HOME_DOT_FILES[@]}"; do
+        add_line_to_file "$file"
+        echo "############################################################################################################################"
+    done
+
+}
+
+add_all_lines_to_all_files
 
 
 
--- scripts/main.sh
+++ scripts/main.sh
@@ -2,9 +2,10 @@
 
 ARCH_PACKAGES_SCRIPT=add_arch_packages.sh
 CUSTOM_CONF_SCRIPT=custom_apps_config.sh
+DOT_OS_CONFIGS=dot_os_configs.sh
 CURRENT_DIR=$(pwd)
-echo "Trying to source $ARCH_PACKAGES_SCRIPT and $CUSTOM_CONF_SCRIPT"
+echo "Trying to source $ARCH_PACKAGES_SCRIPT, $CUSTOM_CONF_SCRIPT and $DOT_OS_CONFIGS"
 
-if [[ -f ./"${ARCH_PACKAGES_SCRIPT}"  &&  -x ./"${ARCH_PACKAGES_SCRIPT}"  && -f ./"${CUSTOM_CONF_SCRIPT}" && -x ./"${CUSTOM_CONF_SCRIPT}" ]]; then 
-    . ./"${ARCH_PACKAGES_SCRIPT}" && . ./"${CUSTOM_CONF_SCRIPT}"
+if [[ -f ./"${ARCH_PACKAGES_SCRIPT}"  &&  -x ./"${ARCH_PACKAGES_SCRIPT}"  && -f ./"${CUSTOM_CONF_SCRIPT}" && -x ./"${CUSTOM_CONF_SCRIPT}" && -f ./"${DOT_OS_CONFIGS}" && -x ./"${DOT_OS_CONFIGS}" ]]; then 
+    . ./"${ARCH_PACKAGES_SCRIPT}" && . ./"${CUSTOM_CONF_SCRIPT}" && . ./"${DOT_OS_CONFIGS}"
 fi
